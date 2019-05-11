5a2810f7cd1f6334e5ce28c34427fe150da7e0cd
michael <michael@michaeldeMacBook-Pro.local>
2019-05-12 02:49:17 +0800

    Initial commit

---
+++ src/main/java/com/bestwehotel/cc/ewechat/EwechatApplication.java
@@ -0,0 +1,13 @@
+package com.bestwehotel.cc.ewechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EwechatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EwechatApplication.class, args);
+    }
+
+}
